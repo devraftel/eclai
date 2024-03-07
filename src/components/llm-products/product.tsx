4fab678528e03eac7e891b24602c1a102c024cdb
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-07 17:40:19 +0500

    Refactor product and required certificates components

--- src/components/llm-products/product.tsx
+++ src/components/llm-products/product.tsx
@@ -29,41 +29,46 @@ export function Product({
 	//   manufacturing_materials: [ 'Glass', 'Aluminum' ]
 	// }
 	return (
-		<div className='p-4 border rounded-xl bg-esl-pm-sec flex flex-col'>
-			<div className='flex items-start gap-2'>
-				<h1 className='text-esl-bg text-xl'>{product_title}</h1>
-				<Chip className='text-xs font-lato float-right '>
-					<Link
-						href={product_url}
-						className='hover:underline no-underline'
-					>
-						View Product
-					</Link>
-				</Chip>
-			</div>
+		<div className='flex flex-col w-full '>
+			<div className='p-4 border rounded-xl bg-esl-pm-sec flex flex-col'>
+				<div className='flex items-start gap-2'>
+					<h1 className='text-esl-bg text-xl'>{product_title}</h1>
+					<Chip className='text-xs font-lato float-right '>
+						<Link
+							href={product_url}
+							className='hover:underline no-underline'
+						>
+							View Product
+						</Link>
+					</Chip>
+				</div>
 
-			<div className=''>
-				<h2 className='text-3xl my-0 font-bold text-esl-bg'>
-					<span className=''>{currency} </span>
-					{price}
-				</h2>
-				<h3 className='mt-1 text-xs text text-esl-sc'>
-					{company_name ? company_name : 'No company Found'}
-				</h3>
-			</div>
+				<div className=''>
+					<h2 className='text-3xl my-0 font-bold text-esl-bg'>
+						<span className=''>{currency} </span>
+						{price}
+					</h2>
+					<h3 className='mt-1 text-xs text text-esl-sc'>
+						{company_name ? company_name : 'No company Found'}
+					</h3>
+				</div>
 
-			<div className='flex flex-wrap gap-2 mt-4 md:mt-8'>
-				{image_url.map((url: string, index: number) => (
-					<Image
-						key={index}
-						src={url}
-						alt={product_title}
-						width={200}
-						height={200}
-						className='rounded-md my-0'
-					/>
-				))}
+				<div className='flex flex-wrap gap-2 mt-4 md:mt-8'>
+					{image_url.map((url: string, index: number) => (
+						<Image
+							key={index}
+							src={url}
+							alt={product_title}
+							width={200}
+							height={200}
+							className='rounded-md my-0'
+						/>
+					))}
+				</div>
 			</div>
+			<p className=''>
+				Finding what ECO Certifications this product shall have :D
+			</p>
 		</div>
 	);
 }
--- src/components/llm-products/req-certificates.tsx
+++ src/components/llm-products/req-certificates.tsx
@@ -4,28 +4,34 @@ export function RequiredCertificatesUI(
 	ecoCertifications: AllRequiredCertificatesProps
 ) {
 	return (
-		<div className='p-4 text-esl-bg border rounded-xl bg-esl-pm-sec font-nunitoSans'>
-			<div className='text-xl pb-2 font-semibold text-white text-balance font-lato'>
-				Required ECO Certifications
+		<div className='flex flex-col w-full'>
+			<div className='p-4 text-esl-bg border rounded-xl bg-esl-pm-sec font-nunitoSans'>
+				<div className='text-xl pb-2 font-semibold text-white text-balance font-lato'>
+					Required ECO Certifications
+				</div>
+				<ul className='list-none m-0 p-0'>
+					{ecoCertifications.certifications.map((certification, index) => {
+						return (
+							<li key={index}>
+								<div className='text-lg font-semibold font-nunitoSans text-esl-bg mt-2'>
+									{index + 1}- {certification.certification_name}
+								</div>
+								<div className='text-sm py-2 text-zinc-300 font-nunitoSans'>
+									{certification.reason}
+								</div>
+								{/* A LINE */}
+								{index < ecoCertifications.certifications.length - 1 && (
+									<div className='w-full border bg-esl-bg border-esl-bg opacity-30 my-1' />
+								)}
+							</li>
+						);
+					})}
+				</ul>
 			</div>
-			<ul className='list-none m-0 p-0'>
-				{ecoCertifications.certifications.map((certification, index) => {
-					return (
-						<li key={index}>
-							<div className='text-lg font-semibold font-nunitoSans text-esl-bg mt-2'>
-								{index + 1}- {certification.certification_name}
-							</div>
-							<div className='text-sm py-2 text-zinc-300 font-nunitoSans'>
-								{certification.reason}
-							</div>
-							{/* A LINE */}
-							{index < ecoCertifications.certifications.length - 1 && (
-								<div className='w-full border bg-esl-bg border-esl-bg opacity-30 my-1' />
-							)}
-						</li>
-					);
-				})}
-			</ul>
+			<p className=''>
+				Eco Advisor is researching and verifying the identified Eco
+				Certifications
+			</p>
 		</div>
 	);
 }
